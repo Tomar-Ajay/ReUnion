8a4873c0933a563f9cb3421253e1f52ca1e39bd9
Ajay Tomar <ajay.tomar.chy16@itbhu.ac.in>
2022-02-26 17:30:47 +0530

    One Change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect('mongodb+srv://frosh:dInJ1EskBg0Ff2Oz@cluster0.jopij.mongodb.net/reuniondb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 },
